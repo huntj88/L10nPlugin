ea644c1bbf3c38009990e259bd5b2f24e64be8be
James Hunt <jhunt@inmotionsoftware.com>
2018-09-03 03:16:29 -0500

    adds the generated source set automatically :D

--- src/main/kotlin/me/jameshunt/l10n/L10nPlugin.kt
+++ src/main/kotlin/me/jameshunt/l10n/L10nPlugin.kt
@@ -1,10 +1,11 @@
 package me.jameshunt.l10n
 
+import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
 
-class L10nPlugin: Plugin<Project> {
+class L10nPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         println("apply L10n")
@@ -14,13 +15,31 @@ class L10nPlugin: Plugin<Project> {
 
         //todo: check hash of strings.xml to see if it changed, and if not do nothing
         val xmlData = StringsXmlParser(project.name).getXmlData()
+
         L10nGenerator(xmlData, generatedSrcPath).generateCode()
+
+        val sourceSets = (project.extensions.getByName("android") as BaseExtension).sourceSets.asMap
+        sourceSets.forEach { sourceSetName, sourceSet ->
+
+            if (sourceSetName == "main") {
+                println(sourceSet.java.srcDirs)
+                val generatedSrcPathSrcSet = "./build/generated/source/L10n/src"
+                println("maybe add: ${File(generatedSrcPathSrcSet).absolutePath}")
+                sourceSet.java.srcDirs(File(generatedSrcPathSrcSet))
+
+                println(sourceSet.java.srcDirs.toString())
+            }
+        }
+
     }
 
     private fun setupGeneratedSourceDirectory(generatedSrcPath: String) {
 
-        val wallpaperDirectory = File(generatedSrcPath)
-        if (wallpaperDirectory.exists()) return
-        wallpaperDirectory.mkdirs()
+        val generatedSourceFolder = File(generatedSrcPath)
+
+        println(generatedSourceFolder.absolutePath)
+
+        if (generatedSourceFolder.exists()) return
+        generatedSourceFolder.mkdirs()
     }
 }
